c23466152d1b84f59747872291b8c5c5be529b6b
TonnyMusembi <musembutonnyj@gmail.com>
2022-09-20 17:53:16 +0300

    add

---
+++ app/Http/Controllers/QrCodeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QrCodeController extends Controller
+{
+
+    public function index(){
+    return view('qrcode');
+
+
+    }
+    public function store(Request $request){
+        return response()->json();
+
+
+    }
+}
