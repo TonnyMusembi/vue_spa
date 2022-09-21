851c2cc0a33d4796a38b46f0d563c22a00a7473f
TonnyMusembi <musembutonnyj@gmail.com>
2022-09-21 12:21:16 +0300

    add

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+
+class UserController extends Controller
+{
+    //
+     public function index(){
+         $users = User::paginate(10);
+    	return response()->json($users);
+    }
+    public function store(Request $request){
+
+
+    }
+
+}
---
+++ resources/views/mails/testMail.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tonny Tech</title>
+</head>
+<body>
+    <div >
+    <p>Hi</p>
+    <p>This is test queue mail.</p>
+    <strong>Thank you</strong>
+    </div>
+</body>
+</html>
