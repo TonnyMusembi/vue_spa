3690c2cd2ebedc0d2445e6a21763153761b50ab7
TonnyMusembi <musembutonnyj@gmail.com>
2022-09-27 10:20:28 +0300

    add

--- resources/views/ImportFile.blade.php
+++ resources/views/ImportFile.blade.php
@@ -49,9 +49,6 @@
 </html>
 <script>
 
-    const list = [1,2,23,3]
-    console.log(list);
-
     const api = 'http://vue-spa.test/api/programs'
     fetch ('api',
    {
@@ -67,6 +64,15 @@
 
      const timeNow = new Date ();
      console.log(timeNow);
+
+
+     let numbers = [-23,-20,-17, -12, -5, 0, 1, 5, 12, 19, 20];
+
+let positive_array = numbers.filter(function(value) {
+    return value >= 0; });
+
+console.log(positive_array);
+
 
 //     const options = {
 //   method: 'GET',
