aade431c86e90d3fecb56b72db6ade0dc786f0ac
TonnyMusembi <musembutonnyj@gmail.com>
2022-09-27 14:42:41 +0300

    add

---
+++ app/Http/Controllers/API/PhoneController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Phone;
+use Illuminate\Http\Request;
+
+class PhoneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+           $phones = Phone::all();
+    return response()->json([
+        'phones' => $phones
+    ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $phone = Phone::create($request->all());
+
+    return response()->json([
+        'message' => "Phone saved successfully!",
+        'phone' => $phone
+    ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Phone  $phone
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Phone $phone)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Phone  $phone
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Phone $phone)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Phone  $phone
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Phone $phone)
+    {
+        //
+        $phone->update($request->all());
+    return response()->json([
+        'message' => "Phone updated successfully!",
+        'product' => $phone
+    ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Phone  $phone
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Phone $phone)
+    {
+        //
+        return response()->json([
+        'message' => "Phone deleted successfully!",
+    ], 200);
+    }
+}
