10fdd49f6022fd1129c1d0ede592f739a50b126c
TonnyMusembi <musembutonnyj@gmail.com>
2022-09-29 17:30:10 +0300

    add

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -14,7 +14,7 @@ class CountryController extends Controller
      */
     public function index()
     {
-        //
+
         $countries = Country::all();
         return response()->json($countries);
     }
@@ -26,7 +26,7 @@ class CountryController extends Controller
      */
     public function create()
     {
-        //
+    // 
     }
 
     /**
