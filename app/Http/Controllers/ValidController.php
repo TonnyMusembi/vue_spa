a5045bd4272803f264c2cac9cd75c4b4fa9bc32f
TonnyMusembi <musembutonnyj@gmail.com>
2022-10-12 11:51:45 +0300

    add

---
+++ app/Http/Controllers/ValidController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Valid;
+use Illuminate\Http\Request;
+
+class ValidController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Valid  $valid
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Valid $valid)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Valid  $valid
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Valid $valid)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Valid  $valid
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Valid $valid)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Valid  $valid
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Valid $valid)
+    {
+        //
+    }
+}
